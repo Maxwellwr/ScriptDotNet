b8e0d537311e538690decffcbb1915bcebd0b628
Игорёк <ZeroDX@mail.ru>
2019-02-01 15:57:53 +0300

    Update Packet.cs

--- src/ScriptDotNetCore/Network/Packet.cs
+++ src/ScriptDotNetCore/Network/Packet.cs
@@ -16,7 +16,7 @@ namespace ScriptDotNet2.Network
         }
 
         public PacketType Method;
-        public short DataLength { get { return (uint)Data.Length; } }
+        public uint DataLength { get { return (uint)Data.Length; } }
         public byte[] Data;
         public byte[] UnusedData;
         
